076fb8215825ce39970a096843bba63bc40cdc02
Yassine Berqiqch <yberqiqch@tc-s.eu>
2024-03-08 11:39:46 +0100

    add log Success Info

--- internal/domain/appModel.go
+++ internal/domain/appModel.go
@@ -6,5 +6,4 @@ type App struct {
 	AppURL               string        `json:"app_url"`
 	Recipients           []string      `json:"recipients"`
 	CheckIntervalSeconds time.Duration `json:"check_interval_seconds"`
-	SendEmailSeconds     time.Duration `json:"send_email_seconds"`
 }
--- internal/healthchecker/healthchecker.go
+++ internal/healthchecker/healthchecker.go
@@ -48,112 +48,106 @@ type HealthResponse struct {
 func (self *HealthChecker) check(app domain.App) {
 	for {
 		tc := time.NewTicker(app.CheckIntervalSeconds * time.Second)
-		//Start timer for 3 seconds
-		MyTimer := time.NewTimer(app.SendEmailSeconds * time.Second)
 		<-tc.C
 		u, err := url.Parse(app.AppURL)
+		logcs.Debug(u.String())
 		if err != nil {
-			logcs.Error(err)
+			logcs.Fatal(err)
 			continue
 		}
 		authResourceClient := self.authClient.NewResourceClient(fmt.Sprintf("%s://%s", u.Scheme, u.Hostname()))
 		resp, err := authResourceClient.GET("", u.Path)
 		if err != nil {
-			logcs.Error(err)
+			logcs.Fatal(err)
 			continue
+		} else {
+			logcs.Success("Your request was successfully submitted")
 		}
-		//create Map to store the health response
-		var healthResponseMap = make(map[string]*HealthResponse)
-		var previousResponce = make(map[string]*HealthResponse)
-		if len(previousResponce) == 0 {
-			previousResponce = healthResponseMap
-		}
-		//if resp.IsError() && resp.IsInternalServerError() {
-		var jsonResponse map[string]interface{}
-		err = json.Unmarshal(resp.Body(), &jsonResponse)
-		if err != nil {
-			logcs.Error(err)
-			continue
-		}
-		if jsonResponse["status"] == "nok" || jsonResponse["status"] == "warn" {
-			// iterate by the slice of string for ordered the map of the response json
-			for _, orderedMap := range jsonOrder {
-				// iterate by the json response
-				for responseKey, responseValue := range jsonResponse {
-					// ordered the map json to check first the status -> info -> error -> details
-					if responseKey == orderedMap {
-						if responseValueMap, ok := responseValue.(map[string]interface{}); ok {
-							// range by Element info to check which component is not ok
-							for componentName, componentInfo := range responseValueMap {
-								if componentInfoMap, yes := componentInfo.(map[string]interface{}); yes {
-									// check the value of status of the component if not ok or warn
-									if componentInfoMap["status"] == "nok" || componentInfoMap["status"] == "warn" {
-										// initialise the map
-										healthResponse, ok := healthResponseMap[componentName]
-										if !ok {
+		if resp.IsError() && resp.IsInternalServerError() {
+			logcs.Info("Http Response Error / Status Code > 399 or == 500")
+			//create Map to store the health response
+			var healthResponseMap = make(map[string]*HealthResponse)
+			var jsonResponse map[string]interface{}
+			err = json.Unmarshal(resp.Body(), &jsonResponse)
+			if err != nil {
+				logcs.Fatal(err)
+				continue
+			} else {
+				logcs.Success("Response body is Unmarshalled successfully")
+			}
+			//jsonFile, err := os.Open("./temp/response.json")
+			//if err != nil {
+			//	logcs.Fatal(err)
+			//	return
+			//}
+			//logcs.Success("Testing File is Opened")
+			//byteValue, _ := ioutil.ReadAll(jsonFile)
+			//err = json.Unmarshal(byteValue, &jsonResponse)
+			//if err != nil {
+			//	logcs.Fatal(err)
+			//	return
+			//}
+			//logcs.Success("File is Unmarshalled successfully")
+			if jsonResponse["status"] == "nok" || jsonResponse["status"] == "warn" {
+				logcs.Info("Response status ist not ok")
+				// iterate by the slice of string for ordered the map of the response json
+				for _, orderedMap := range jsonOrder {
+					// iterate by the json response
+					for responseKey, responseValue := range jsonResponse {
+						// ordered the map json to check first the status -> info -> error -> details
+						if responseKey == orderedMap {
+							if responseValueMap, ok := responseValue.(map[string]interface{}); ok {
+								// range by Element info to check which component is not ok
+								for componentName, componentInfo := range responseValueMap {
+									if componentInfoMap, yes := componentInfo.(map[string]interface{}); yes {
+										// check the value of status of the component if not ok or warn
+										if componentInfoMap["status"] == "nok" || componentInfoMap["status"] == "warn" {
+											logcs.Info("One component status is not ok")
+											// initialise the map
+											healthResponse, ok := healthResponseMap[componentName]
+											if !ok {
+												healthResponse = &HealthResponse{}
+											}
 											healthResponse = &HealthResponse{}
+											healthResponseMap[componentName] = healthResponse
+											// put the name and status of component which is not ok in map
+											healthResponse.Name = componentName
+											healthResponse.Status = fmt.Sprintf("%s", componentInfoMap["status"])
 										}
-										healthResponse = &HealthResponse{}
-										healthResponseMap[componentName] = healthResponse
-										// put the name and status of component which is not ok in map
-										healthResponse.Name = componentName
-										healthResponse.Status = fmt.Sprintf("%s", componentInfoMap["status"])
 									}
-								}
-								// check if there is an error for the component which is not ok
-								if _, ok := healthResponseMap[componentName]; ok && responseKey == "error" {
-									healthResponseMap[componentName].Error = fmt.Sprintf("%v", componentInfo)
-								}
-								// check if there is an details for the component which is not ok
-								if _, ok := healthResponseMap[componentName]; ok && responseKey == "details" {
-									healthResponseMap[componentName].Details = map[string]interface{}{
-										componentName: componentInfo,
+									// check if there is an error for the component which is not ok
+									if _, ok := healthResponseMap[componentName]; ok && responseKey == "error" {
+										healthResponseMap[componentName].Error = fmt.Sprintf("%v", componentInfo)
+									}
+									// check if there is an details for the component which is not ok
+									if _, ok := healthResponseMap[componentName]; ok && responseKey == "details" {
+										healthResponseMap[componentName].Details = map[string]interface{}{
+											componentName: componentInfo,
+										}
 									}
 								}
 							}
 						}
 					}
 				}
-			}
-			// testing if the last map not equal a new map response
-			if ok := compareMaps(healthResponseMap, previousResponce); ok {
-				formatEmail(healthResponseMap)
-				continue
-			} else {
-				select {
-				case <-MyTimer.C:
-					formatEmail(healthResponseMap)
-					break
-				default:
-					continue
+				// Formatting Error Email
+				for _, value := range healthResponseMap {
+					if len(value.Error) == 0 {
+						value.Error = "Error not found"
+					}
+					if len(value.Details) == 0 {
+						value.Details = map[string]interface{}{
+							"Details": "not found",
+						}
+					}
+					err := fmt.Errorf("App name is : %s\n, Name : %s\n, Error : %s\n, Details is : %s\n", u.Hostname(), value.Name, value.Error, value.Details)
+					logcs.Error(err)
 				}
-				<-time.After(app.SendEmailSeconds * time.Second)
+			} else {
+				logcs.Info("Status Response ist Ok")
 			}
-			previousResponce = healthResponseMap
-			fmt.Println("map after ", previousResponce)
+		} else {
+			logcs.Success("Http Response ist Ok")
 		}
-	}
-}
-
-// Compare the Error in the two Map, and return true when not equal
-func compareMaps(nextMap, prevMap map[string]*HealthResponse) bool {
-	for i, v := range nextMap {
-		if v.Error != prevMap[i].Error {
-			return true
-		}
-	}
-	return false
-}
-
-// formatting email and send it with logcs
-func formatEmail(healthResponseMap map[string]*HealthResponse) {
-	for _, value := range healthResponseMap {
-		if len(value.Details) == 0 {
-			value.Details = map[string]interface{}{
-				"Details": "not found",
-			}
-		}
-		err := fmt.Errorf("App name is : %s\n, Name : %s\n, Error : %s\n, Details is : %s\n", value.AppUrl, value.Name, value.Error, value.Details)
-		logcs.Error(err)
 	}
 }
